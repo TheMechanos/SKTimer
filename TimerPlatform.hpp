102d8daffd9bb844915061e939a0ee01f561c294
Sebastian Koczur <themechanos@gmail.com>
2025-09-24 10:53:33 +0200

    Add ESP platform

--- TimerPlatform.hpp
+++ TimerPlatform.hpp
@@ -1,10 +1,21 @@
 #pragma once
 
 
-#ifdef ARDUINO
+#if defined(ARDUINO)
 
 #include <Arduino.h>
 typedef unsigned long Tick_t;
 inline Tick_t getTick() { return millis(); }
 
+
+
+#elif defined(ESP_PLATFORM)
+
+#include <esp_timer.h>
+typedef uint32_t Tick_t;
+inline Tick_t getTick() { return esp_timer_get_time() / 1000; }
+
+
+
 #endif
+
