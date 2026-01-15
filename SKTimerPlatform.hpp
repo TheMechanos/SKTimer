296800b4a2dc1608446681d8a162dc74cff7c531
Sebastian Koczur <s.koczur@corab.com.pl>
2026-01-15 08:52:39 +0100

    Add STM32C0 support

--- SKTimerPlatform.hpp
+++ SKTimerPlatform.hpp
@@ -20,4 +20,14 @@ inline Tick_t getTick() { return esp_timer_get_time() / 1000; }
 }
 
 
+#elif defined(STM32C0)
+
+#include <stm32c0xx_hal.h>
+
+namespace SK {
+typedef uint32_t Tick_t;
+inline Tick_t getTick() { return HAL_GetTick(); }
+}
+
+
 #endif
